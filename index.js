5dd07eff51d57c42262d02ec2691c61d35b337e5
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2022-04-01 13:46:01 -0300

    dotenv

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
-
+console.log(process.env.DISCORD_TOKEN)
 client.on('message', (message) =>{
   if(message.author.username =='gabrielomn')
     console.log(message)
     message.react('💩')
 })
 
-client.login('OTU2MzM0MjEwMjc5NDQwNTI1.Yjutow.ZGfmOlnt0QBy9jIhEh1H5Cw3krk')
+client.login(process.env.DISCORD_TOKEN)
 
