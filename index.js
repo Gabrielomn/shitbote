56f4686a72ce8acfcb72862c4f75370dc57de6fe
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2022-04-01 14:12:33 -0300

    add reactions and stuff

--- index.js
+++ index.js
@@ -2,11 +2,51 @@ require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
-console.log(process.env.DISCORD_TOKEN)
+const reacoes = [
+  [
+    '🇴',
+    '🇹',
+    '🇦',
+    '🇷',
+    '🇮',
+    '🅾️'
+  ],
+  [
+    '🇲',
+    '🇪',
+    '🇷',
+    '🇩',
+    '🇦',
+  ],
+  [
+    '🇱',
+    '🇮',
+    '🇽',
+    '🇴',
+  ],
+  ['💩']
+]
+
+qtdReacoes = reacoes.length
+
+getRandomReacao = () => {
+  index = Math.floor(Math.random() * qtdReacoes)
+  return reacoes[index]
+}
+
 client.on('message', (message) =>{
-  if(message.author.username =='gabrielomn')
-    console.log(message)
-    message.react('💩')
+  if(message.author.username == process.env.TARGET){
+    const reacao = getRandomReacao()
+    let cont = 0
+    reacao.forEach((emoji) => {
+      setTimeout(() => {
+        message.react(emoji)
+
+      }, cont * 10)
+      cont++
+    })
+  }
+
 })
 
 client.login(process.env.DISCORD_TOKEN)
